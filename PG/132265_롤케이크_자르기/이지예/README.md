623f97514b023b02f9eb27497c36041c7ac58105
lj05117 <lj05117@naver.com>
2023-04-18 21:54:23 +0900

    롤케이크 자르기

---
+++ PG/132265_롤케이크_자르기/이지예/README.md
@@ -0,0 +1,2 @@
+![image](https://user-images.githubusercontent.com/58285947/232783530-39be50de-6bf2-4d00-93ab-9cb06c41ba4f.png)
+![image](https://user-images.githubusercontent.com/58285947/232783616-495e3dad-34ab-4bd2-acc9-fa7a3faffe09.png)
--- PG/132265_롤케이크_자르기/이지예/solution.js
+++ PG/132265_롤케이크_자르기/이지예/solution.js
@@ -1,46 +1,27 @@
-// const solution = (topping) => {
-//   let answer = 0;
-//   for (let i = 1; i < topping.length - 1; i++) {
-//     let leftCount = countTopping(0, i, topping);
-//     let rightCount = countTopping(i, topping.length, topping);
-//     if (leftCount === rightCount) answer++;
-//     else if (leftCount > rightCount) break;
-//   }
-//   console.log(answer);
-//   return answer;
-// };
-
-// const countTopping = (start, end, topping) => {
-//   const eatableToppingList = [];
-//   let result = 0;
-//   for (let i = start; i < end; i++) {
-//     if (!eatableToppingList.includes(topping[i])) {
-//       eatableToppingList.push(topping[i]);
-//       result++;
-//     }
-//   }
-//   return result;
-// };
-function solution(topping) {
+const solution = (topping) => {
   let answer = 0;
-  for (let i = 1; i < topping.length - 1; i++) {
-    let leftCount = countTopping(0, i, topping);
-    let rightCount = countTopping(i, topping.length, topping);
-    if (leftCount === rightCount) answer++;
-    else if (leftCount > rightCount) break;
+  const allTopping = new Map();
+  const bro = new Map();
+
+  topping.forEach((n) => {
+    //topping 각각의 원소를 받아와서
+    allTopping.set(n, (allTopping.get(n) || 0) + 1);
+    //받아온 원소의 값에 +1해준다. 기존에 현재 원소를 담아놓지 않았다면 기존 값을 받아오지 않고 0으로 설정 해 준다.
+  });
+
+  for (let n of topping) {
+    //각각의 원소를
+    allTopping.set(n, allTopping.get(n) - 1); //allTopping에서 해당 원소 하나씩 뺀다.
+    bro.set(n, true); //bro라는 Map 자료구조에 n을 true로 설정한다.
+    if (!allTopping.get(n)) allTopping.delete(n);
+    //n이 남은 토핑에 없으면 아예 없애준다.
+
+    if (allTopping.size === bro.size) answer++;
+    //남은 토핑이랑 뺀 토핑의 갯수가 같으면 정답에 +해준다.
+
+    if (allTopping.size < bro.size) break;
+    //뺀 토핑 갯수가 더 많아지면 아예 멈춘다.
   }
   return answer;
-}
-
-function countTopping(start, end, topping) {
-  const eatableToppingList = [];
-  let result = 0;
-  for (let i = start; i < end; i++) {
-    if (!eatableToppingList.includes(topping[i])) {
-      eatableToppingList.push(topping[i]);
-      result++;
-    }
-  }
-  return result;
-}
-solution([1, 2, 3, 1, 4]);
+};
+console.log(solution([1, 2, 1, 3, 1, 4, 1, 2]));
